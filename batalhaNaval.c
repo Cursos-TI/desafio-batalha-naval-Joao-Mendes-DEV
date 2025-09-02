056e6c197e95beab8f66a116039e5c5fffa11fb3
Joao-Mendes-DEV <joaomfmendes@hotmail.com.br>
2025-09-02 17:03:37 -0300

    batalhaNaval.c

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,23 +1,67 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main (){
+#define TAM 10
+#define NAVIO 3
+#define AGUA 0
+#define TAM_NAVIO 3
 
-    int tabuleiro[10][10] = { // Inicialização do array com valores
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 3, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 3, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 3, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 3, 3, 3, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-    };
- 
-    // Saída dos dados que representa o tabuleiro
-    for (int i = 0; i < 10; i++){
-        for (int j = 0; j < 10; j++) {
+int main() {
+    int tabuleiro[TAM][TAM];  
+    int navio[TAM_NAVIO] = {NAVIO, NAVIO, NAVIO};
+
+    // 1. Inicializar o tabuleiro com água
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM; j++) {
+            tabuleiro[i][j] = AGUA;
+        }
+    }
+
+    // Navio horizontal
+    int linhaH = 2, colunaH = 1; 
+    for (int i = 0; i < TAM_NAVIO; i++) {
+        tabuleiro[linhaH][colunaH + i] = NAVIO;
+    }
+
+    // Navio vertical
+    int linhaV = 5, colunaV = 7; 
+    for (int i = 0; i < TAM_NAVIO; i++) {
+        tabuleiro[linhaV + i][colunaV] = NAVIO;
+    }
+
+    // Navio diagonal
+    int linhaD1 = 7, colunaD1 = 1;  
+    int valido = 1;
+    for (int i = 0; i < TAM_NAVIO; i++) {
+        if (linhaD1 + i >= TAM || colunaD1 + i >= TAM || tabuleiro[linhaD1 + i][colunaD1 + i] == NAVIO) {
+            valido = 0;
+            break;
+        }
+    }
+    if (valido) {
+        for (int i = 0; i < TAM_NAVIO; i++) {
+            tabuleiro[linhaD1 + i][colunaD1 + i] = NAVIO;
+        }
+    }
+
+    // Navio diagonal
+    int linhaD2 = 1, colunaD2 = 6;  
+    valido = 1;
+    for (int i = 0; i < TAM_NAVIO; i++) {
+        if (linhaD2 + i >= TAM || colunaD2 - i < 0 || tabuleiro[linhaD2 + i][colunaD2 - i] == NAVIO) {
+            valido = 0;
+            break;
+        }
+    }
+    if (valido) {
+        for (int i = 0; i < TAM_NAVIO; i++) {
+            tabuleiro[linhaD2 + i][colunaD2 - i] = NAVIO;
+        }
+    }
+
+    // EXIBIR TABULEIRO
+    printf("=== TABULEIRO BATALHA NAVAL ===\n\n");
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM; j++) {
             printf("%d ", tabuleiro[i][j]);
         }
         printf("\n");
